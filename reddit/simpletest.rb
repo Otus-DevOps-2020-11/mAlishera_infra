ebad11e78fe718dc824a6271b72df5ba0d404d66
Ekaterina Shemerey <ekshemerey@ozon.ru>
2021-03-08 16:49:50 +0300

    sinatra changes

--- reddit/simpletest.rb
+++ reddit/simpletest.rb
@@ -5,14 +5,14 @@ require 'rack/test'
 set :environment, :test
 
 class MyAppTest < Test::Unit::TestCase
-  include Rack::Test::Methods
+    include Rack::Test::Methods
 
-  def app
-    Sinatra::Application
-  end
+    def app
+        Sinatra::Application
+    end
 
-  def test_get_request
-    get '/'
-    assert last_response.ok?
-  end
+    def test_get_request
+        get '/'
+        assert last_response.ok?
+    end
 end
